f1a39df716bc4938703b953ed868996da2ebce5c
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 06:29:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,15 @@
 class Application
 
-  def call
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, World"
 
+    if Time.now.strftime("%p") == "AM"
+      resp.write "Good Morning"
+    elsif Time.now.strftime("%p") == "PM"
+      resp.write "Good Afternoon"
+    end
+    resp.finish
   end
 
 end
